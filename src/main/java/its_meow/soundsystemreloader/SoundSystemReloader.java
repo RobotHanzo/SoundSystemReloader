d75c64e01f98351f302be532440bd2beb7af4f09
itsmeow <mods.itsmeow@gmail.com>
2019-06-17 09:23:01 -0400

    Add check for loaded and status bar notifications

--- src/main/java/its_meow/soundsystemreloader/SoundSystemReloader.java
+++ src/main/java/its_meow/soundsystemreloader/SoundSystemReloader.java
@@ -5,6 +5,9 @@ import org.apache.logging.log4j.Logger;
 import org.lwjgl.input.Keyboard;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
@@ -20,12 +23,20 @@ public class SoundSystemReloader {
     public static final String acceptedMCV = "[1.12,1.12.2]";
 
     public static final Logger LOGGER = LogManager.getLogger(SoundSystemReloader.MOD_ID);
+    
+    public static final Style GREEN_STYLE = new Style().setColor(TextFormatting.GREEN);
+    public static final Style RED_STYLE = new Style().setColor(TextFormatting.RED);
 
     @SubscribeEvent
     public static void keyPress(KeyInputEvent event) {
         if(Keyboard.isKeyDown(Keyboard.KEY_F3) && Keyboard.isKeyDown(Keyboard.KEY_S)) {
-            LOGGER.info("Captured F3+S: Reloading Sound System...");
-            Minecraft.getMinecraft().getSoundHandler().sndManager.reloadSoundSystem();
+            if(Minecraft.getMinecraft().getSoundHandler().sndManager.loaded) {
+                LOGGER.info("Captured F3+S: Reloading Sound System...");
+                Minecraft.getMinecraft().player.sendStatusMessage(new TextComponentString("Reloading sound system").setStyle(GREEN_STYLE), true);
+                Minecraft.getMinecraft().getSoundHandler().sndManager.reloadSoundSystem();
+            } else {
+                Minecraft.getMinecraft().player.sendStatusMessage(new TextComponentString("Unable to reload sound system, it is not loaded").setStyle(RED_STYLE), true);
+            }
         }
     }
 
